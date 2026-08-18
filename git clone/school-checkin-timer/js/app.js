4b289b418b7179ee8c1225d2b4fefbfe777211af
bantai-education-design <ban.tai3educationdesign@gmail.com>
2026-08-18 23:22:16 +0900

    feat: send heart weather to teacher dashboard

--- git clone/school-checkin-timer/js/app.js
+++ git clone/school-checkin-timer/js/app.js
@@ -1,15 +1,13 @@
 (() => {
-  const weatherButtons = [...document.querySelectorAll('.weather')];
-
-  weatherButtons.forEach((button) => {
-    button.setAttribute('aria-pressed', 'false');
-    button.addEventListener('click', () => {
-      weatherButtons.forEach((item) => {
-        item.classList.remove('selected');
-        item.setAttribute('aria-pressed', 'false');
-      });
-      button.classList.add('selected');
-      button.setAttribute('aria-pressed', 'true');
+  const weatherButtons=[...document.querySelectorAll('.weather')];
+  weatherButtons.forEach((button,index)=>{
+    const weather=['sunny','cloudy','rainy','thunder'][index];
+    button.dataset.weather=weather;
+    button.setAttribute('aria-pressed','false');
+    button.addEventListener('click',()=>{
+      weatherButtons.forEach(item=>{item.classList.remove('selected');item.setAttribute('aria-pressed','false');});
+      button.classList.add('selected');button.setAttribute('aria-pressed','true');
+      window.MorningTasks?.setWeather(weather);
     });
   });
 })();
